b63280a94b453b36187dd60c160020f4e2771c0a
viktorholk <holkinator@gmail.com>
2025-05-05 16:56:28 +0200

    Added tailwind & shadcn components

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,17 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
