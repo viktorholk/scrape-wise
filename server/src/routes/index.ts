8f39f57a334b416c095a3ba693595345b0d51cb8
viktorholk <holkinator@gmail.com>
2025-05-07 20:33:22 +0200

    added jobs router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 import usersRouter from "./users";
 import authRouter from "./auth";
 import crawlerRouter from "./crawler";
+import jobsRouter from "./jobs";
 
 const router = Router();
+
 router.use('/auth',  authRouter)
 router.use('/users', usersRouter)
 router.use('/crawler', crawlerRouter)
+router.use('/jobs', jobsRouter)
 
 export default router;
---
+++ server/src/routes/jobs.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/lib/database";
+import { requestHandler } from "@/lib/utils";
+import { Router, Request, Response } from "express";
+import { authMiddleware } from "@/middlewares/auth";
+
+const router = Router();
+
+router.get('/', authMiddleware, requestHandler(async (req: Request, res: Response): Promise<void> => {
+
+    const userId = req.userId!;
+
+    const jobs = await prisma.crawlerJob.findMany({
+        where: {
+            userId
+        },
+        select: {
+            id: true,
+            createdAt: true,
+            status: true,
+            results: true,
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+    });
+
+    res.status(200).json(jobs);
+}));
+
+export default router;
