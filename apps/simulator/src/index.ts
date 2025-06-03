aa5b2b162785bf7b6b84a1c07aba7c6bfe49bb3f
viktorholk <holkinator@gmail.com>
2025-06-03 11:37:34 +0200

    seed script in backend. fixed style for simulator app in prod

--- apps/simulator/src/index.ts
+++ apps/simulator/src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/simulator', express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
